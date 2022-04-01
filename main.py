9c82c4694f9e55d58e21548ea66eb18e259d5e4c
Anatoliy <epquick@gmail.com>
2022-04-02 01:38:35 +0300

    statistics calculating

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ if __name__ == '__main__':
     years = statistics.get_available_years()
     for year in years:
         top_towns_by_sales_ratio = statistics.get_year_top_by_sales_ratio(year, 10)
-        top_towns_by_sales_amount = statistics.get_year_top_by_sales_volume(year, 10)
+        top_towns_by_sales_volume = statistics.get_year_top_by_sales_volume(year, 10)
 
         print('###', year)
         print('Top 10 towns by sales ratio:  ', ', '.join([t.town_name for t in top_towns_by_sales_ratio]))
-        print('Top 10 towns by sales volume: ', ', '.join([t.town_name for t in top_towns_by_sales_amount]))
+        print('Top 10 towns by sales volume: ', ', '.join([t.town_name for t in top_towns_by_sales_volume]))
